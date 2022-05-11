b90965c53af87afc4a54df1d658b3375ac98f06b
maxcaplan <maxacaplan@gmail.com>
2022-05-11 09:50:52 -0300

    Add github api JSON fetch and parsing

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -27,14 +27,58 @@ async function getDataFromFile(filePath: string): Promise<Repo[] | AppError> {
   }
 }
 
+async function getDataFromHTTPS(host: string, path: string): Promise<Repo[] | AppError> {
+  try {
+    var options = {
+      host: host,
+      path: path,
+      method: 'GET',
+      headers: {
+        'User-Agent': 'request'
+      }
+    };
+
+    let data: Repo[] = await new Promise((resolve, reject) => {
+      https.request(options, function (response: IncomingMessage) {
+        let body = ""
+
+        response.on('data', (chunk) => {
+          body += chunk
+        })
+
+        response.on("end", () => {
+          if (response.statusCode === 200) {
+            try {
+              resolve(JSON.parse(body));
+            } catch (err) {
+              reject(err)
+            }
+          } else {
+            reject({ message: response.statusMessage, status: response.statusCode })
+          }
+        })
+      }).on("error", (err: Error) => {
+        reject(err)
+      }).end()
+    })
+
+    return data
+
+  } catch (err) {
+    const message = err instanceof Error ? err.message : "Unable to get file"
+    throw new AppError(message, 500)
+  }
+}
+
 repos.get('/', async (req: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   try {
     let fileData = await getDataFromFile("../../data/repos.json")
+    let apiData = await getDataFromHTTPS('api.github.com', '/users/silverorange/repos')
 
     res.status(200)
-    res.json(fileData)
+    res.json([])
   } catch (err) {
     console.log(err)
     res.status(400)
