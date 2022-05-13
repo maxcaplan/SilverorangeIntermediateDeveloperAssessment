78c0d2bafbb2b5efd0a704e2187083d3a62b790f
maxcaplan <maxacaplan@gmail.com>
2022-05-13 19:07:38 -0300

    Fix modal overflow scrolling on the Y axis

--- web/src/components/Modal/Modal.tsx
+++ web/src/components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 export function Modal(props: any) {
   return (
-    <div className="fixed z-10 w-full h-full bg-gray-900/50">
+    <div className="fixed z-10 w-full min-h-full inset-0 overflow-y-auto bg-gray-900/50">
       <div className="container mx-auto py-3 px-2">{props.children}</div>
     </div>
   );
