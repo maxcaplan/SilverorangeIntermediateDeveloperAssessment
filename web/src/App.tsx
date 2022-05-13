60befc14cdf52ac5aaf0efe7ef5d6eabc331ffc5
maxcaplan <maxacaplan@gmail.com>
2022-05-13 01:11:56 -0300

    Add repo list creation date sorting

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,28 +4,55 @@ import { RepositoryList } from './components/repository/RepositoryList';
 
 import { Repo } from './models/Repo';
 import { AppError } from './models/AppError';
+import { Direction } from './models/Direction';
 
 import './App.css';
+import { Header } from './components/Header/Header';
 
 type AppProps = any;
 
+// origRepos: the original array of repo data from /repos
+// repos: the current array of repo data
 interface AppState {
   isLoading: boolean;
   isFailed: boolean;
   error: AppError | null;
+  dir: Direction;
+  origRepos: Repo[] | null;
   repos: Repo[] | null;
 }
 
 export class App extends React.Component<AppProps, AppState> {
-  constructor(props: any) {
+  constructor(props: AppProps) {
     super(props);
 
     this.state = {
       isLoading: false,
       isFailed: false,
       error: null,
+      dir: 'asc',
+      origRepos: null,
       repos: null,
     };
+  }
+
+  // Sorts repository array by date created
+  // in ascending (asc) or descending (desc) order
+  private sortRepositories(repos: Repo[], order: Direction = 'asc'): Repo[] {
+    return repos.sort((a, b) => {
+      const aTime = new Date(a.created_at).getTime();
+      const bTime = new Date(b.created_at).getTime();
+
+      if (aTime < bTime) {
+        return order === 'asc' ? -1 : 1;
+      }
+
+      if (aTime > bTime) {
+        return order === 'asc' ? 1 : -1;
+      }
+
+      return 0;
+    });
   }
 
   private async getRepositories(url: RequestInfo) {
@@ -43,10 +70,12 @@ export class App extends React.Component<AppProps, AppState> {
       });
 
       const data: Repo[] = await response.json();
+      const sortedData = this.sortRepositories(data, this.state.dir);
 
       this.setState({
         isLoading: false,
-        repos: data,
+        origRepos: data,
+        repos: sortedData,
       });
     } catch (err) {
       const message =
@@ -62,13 +91,31 @@ export class App extends React.Component<AppProps, AppState> {
     }
   }
 
+  private updateDirection = (dir: Direction) => {
+    if (!this.state.repos) {
+      return;
+    }
+
+    const sortedRepos = this.sortRepositories(this.state.repos, dir);
+
+    this.setState({
+      dir,
+      repos: sortedRepos,
+    });
+  };
+
   public componentDidMount() {
     this.getRepositories('http://localhost:4000/repos');
   }
 
   public render() {
     return (
-      <div className="App container mx-auto py-3">
+      <div className="App container mx-auto py-3 px-2">
+        <Header
+          dir={this.state.dir}
+          updateListDirection={this.updateDirection}
+        />
+
         {!this.state.isLoading && !this.state.isFailed && (
           <RepositoryList data={this.state.repos} />
         )}
---
+++ web/src/components/Header/SortButton.tsx
@@ -0,0 +1,29 @@
+import { Button } from '../Button/Button';
+import { SortAscIcon } from '@primer/octicons-react';
+import { SortDescIcon } from '@primer/octicons-react';
+
+import { Direction } from '../../models/Direction';
+
+interface SortButtonProps {
+  dir: Direction;
+  toggleDirection: () => void;
+}
+
+export function SortButton(props: SortButtonProps) {
+  const sortIcon =
+    props.dir === 'asc' ? (
+      <SortAscIcon size="small" verticalAlign="middle" className="mr-1" />
+    ) : (
+      <SortDescIcon size="small" verticalAlign="middle" className="mr-1" />
+    );
+
+  return (
+    <Button
+      className="btn-outlined"
+      handleClick={() => props.toggleDirection()}
+    >
+      {sortIcon}
+      Sort
+    </Button>
+  );
+}
